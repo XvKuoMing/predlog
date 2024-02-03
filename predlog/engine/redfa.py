89dfac4041f2811b4168726d7b32de040f60e38e
Kamil <80534052+XvKuoMing@users.noreply.github.com>
2024-02-03 17:48:11 +0300

    Update redfa.py

--- predlog/engine/redfa.py
+++ predlog/engine/redfa.py
@@ -1,5 +1,14 @@
 from .utils.graphs_builder import regex2dfa
 from copy import deepcopy
+from warnings import warn
+
+
+try:
+    import matplotlib.pyplot as plt
+    import networkx as nx
+except ModuleNotFoundError:
+    warn("To use draw method of ReDFA install metplotlib and networkx")
+    
 
 
 class ReDFA:
@@ -12,11 +21,11 @@ class ReDFA:
         self.__transitions = transition_graph
 
     @property
-    def graph(self):
+    def graph(self) -> dict:
         return deepcopy(self.__transitions)
 
     @property
-    def final_states(self):
+    def final_states(self) -> list:
         return deepcopy(self.__final_states)
 
     def __str__(self):
@@ -38,4 +47,35 @@ class ReDFA:
             else:  # if element is not valid, it's simply false
                 return False
         return current_state in self.__final_states  # check if such end is allowed
+
+    def draw(self, 
+             figzise: tuple = (15, 10), dpi: int = 60,
+             edge_color: str = "black", width: int = 1, linewidths: int =1,
+             node_size: int = 500, node_color: str = 'pink', alpha: float = 0.9, font_color: str = 'red') -> None:
+        """Draws directed graph"""
+        edges = []
+        edge_labels = {}
+        
+        for s1, transit in dfa.graph.items():
+          for label, s2 in transit.items():
+            edge = [s1, s2]
+            edges.append(edge)
+            edge_labels[tuple(edge)] = label
+        
+        G = nx.DiGraph()
+        G.add_edges_from(edges)
+        pos = nx.spring_layout(G)
+        plt.figure(figsize=figzise, dpi=dpi)
+        nx.draw(
+            G, pos, edge_color=edge_color, width=width, linewidths=linewidths,
+            node_size=node_size, node_color=node_color, alpha=alpha,
+            labels={node: node for node in G.nodes()}
+        )
+        nx.draw_networkx_edge_labels(
+            G, pos,
+            edge_labels=edge_labels,
+            font_color=font_color
+        )
+        plt.axis('off')
+        plt.show()
 
